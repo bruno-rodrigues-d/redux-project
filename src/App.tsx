d2f8e377fb397d0d64dbad0683e978861aaffe05
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2024-01-23 15:10:07 -0300

    feat: create a video player layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import './styles/global.css'
+
 import { Provider as ReduxProvider } from "react-redux";
 
-import { AddToDo } from "./components/AddToDo";
-import { ToDoList } from "./components/ToDoList";
 import { store } from "./store";
+import { Player } from "./pages/player";
 
 export function App() {
   return (
     // Sempre será necessário envolver os componentes pelo Provider, para receber as informações globais
     <ReduxProvider store={store}>
-      <ToDoList />
-      <AddToDo />
+      <Player />
     </ReduxProvider>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,10 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
-
-// O Slice seria a fatia do estado, pedaço menor do estado
-const todoSlice = createSlice({
-  name: 'todo',
-  initialState: ['Fazer café', 'Estudar Redux', 'Estudar Zustand'],
-
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo)
-    }
-  }
-})
 
 // O reducer seria as informações que vão ser compartilhadas globalmente
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer
-  },
+  reducer: {},
 })
-
-export const { add } = todoSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
