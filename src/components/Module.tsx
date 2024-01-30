351f3c36df978fc264ea584f48d497d2824b6554
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2024-01-30 11:06:25 -0300

    feat: highlighting  actual lesson

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -15,6 +15,12 @@ interface ModuleProps {
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   const dispatch = useDispatch();
 
+  const { currentModuleIndex, currentLessonIndex } = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    return { currentModuleIndex, currentLessonIndex }
+  })
+
   // Aqui eu recupero do Redux a informação das aulas
   const lessons = useAppSelector(state => {
     return state.player.course.modules[moduleIndex].lessons
@@ -38,11 +44,14 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
           {lessons.map((lesson, lessonIndex) => {
+            const isCurrent = currentModuleIndex === moduleIndex &&
+            currentLessonIndex === lessonIndex
               return (
                 <Lesson
                   key={lesson.id}
                   title={lesson.title}
                   duration={lesson.duration}
+                  isCurrent={isCurrent}
                   onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
                 />
               )
