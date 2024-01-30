adf06fbbe04a1d45bba58c746312e82a89a0e4f4
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2024-01-30 10:45:05 -0300

    feat: create a player reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
+import { player } from "./slices/player";
 
 // O reducer seria as informações que vão ser compartilhadas globalmente
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    player,
+  },
 })
 
 export type RootState = ReturnType<typeof store.getState>;
